a6020b2d00b68e2fc05b9499a0ba7a89b8b439ac
V!RUS <Phan Thanh Hue>
2018-11-10 22:52:19 +0700

    added CRUD and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :category
     validates :title, :content, :category_id, presence: true
+    validates :title, :content, :category, presence: true
+    has_many :line_items, inverse_of: :order
 end
